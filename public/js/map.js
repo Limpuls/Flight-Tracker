e62a2cec5bb629040b82e856fd2c3ff1df07d567
Laimis <laim0039@edu.easj.dk>
2019-05-16 11:16:26 +0200

    geolocat

--- public/js/map.js
+++ public/js/map.js
@@ -2,7 +2,7 @@ var map;
 function initMap() {
     var options = {
         center: {lat: -34.397, lng: 150.644},
-        zoom: 8
+        zoom: 3
     }
 
     map = new google.maps.Map(document.getElementById('map'), options);
@@ -11,4 +11,36 @@ function initMap() {
         position: {lat: 42.4668, lng: 70.9495},
         map: map
     })
+
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+        }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+            'Error: The Geolocation service failed.' :
+            'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+    }
 }
+
+
+
