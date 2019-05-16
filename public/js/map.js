e2e02f48b90fd01e1ac0cefb21e86a89368d5279
Martin Sabkov <martin.sabkov@gmail.com>
2019-05-16 11:29:07 +0200

    Hope it is changed

--- public/js/map.js
+++ public/js/map.js
@@ -1,14 +1,14 @@
 var map;
 function initMap() {
     var options = {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
+        center: {lat: 54.9065, lng: 25.3189},
+        zoom: 4.8
     }
 
     map = new google.maps.Map(document.getElementById('map'), options);
 
     var marker = new google.maps.Marker({
-        position: {lat: 42.4668, lng: 70.9495},
+        position: {lat: 54.9065, lng: 25.3189},
         map: map
     })
 }
